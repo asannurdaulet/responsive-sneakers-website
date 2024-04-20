032ce34134a9bbdd28642e41db1100b65e54f491
asannurdaulet <erlibekelnur@gmail.com>
2024-04-20 09:59:54 +0500

    proektyny ayaktau

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,6 +52,14 @@ let swiperTitles = new Swiper(".home__titles", {
   centeredSlides: true,
 });
 
-swiperImages.controller.control = swiperTitles
-swiperTitles.controller.control = swiperImages
+swiperImages.controller.control = swiperTitles;
+swiperTitles.controller.control = swiperImages;
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const bgHeader = () => {
+  const header = document.getElementById("header");
+  // Add a class if the bottom offset is greater than 50 of the viewport
+  this.scrollY >= 50
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
+};
+window.addEventListener("scroll", bgHeader);
